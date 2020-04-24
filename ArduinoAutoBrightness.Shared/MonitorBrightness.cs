95a9e3b4b07fe5b58fe1bc78717a4445694c09a0
Maksym Koshovyi <maximkoshevoi61@gmail.com>
2020-04-24 23:49:54 +0300

    Minor refactoring

--- ArduinoAutoBrightness.Shared/MonitorBrightness.cs
+++ ArduinoAutoBrightness.Shared/MonitorBrightness.cs
@@ -10,16 +10,16 @@ namespace ArduinoAutoBrightness.Shared
 
         public static int Get()
         {
-            using (var mclass = new ManagementClass("WmiMonitorBrightness"))
-            {
-                mclass.Scope = new ManagementScope(@"\\.\root\wmi");
-                using var instances = mclass.GetInstances();
-                foreach (ManagementObject instance in instances)
-                {
-                    return (byte)instance.GetPropertyValue("CurrentBrightness");
-                }
-            }
-            return 0;
+        using var mclass = new ManagementClass("WmiMonitorBrightness")
+        {
+            Scope = new ManagementScope(@"\\.\root\wmi")
+        };
+        using var instances = mclass.GetInstances();
+        foreach (ManagementObject instance in instances)
+        {
+            return (byte)instance.GetPropertyValue("CurrentBrightness");
+        }
+        return 0;
         }
 
         public static void Set(int brightness)
